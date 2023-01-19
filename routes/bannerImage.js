cbc96e024d1e9108b7af132bdef4716d19436755
Lakshay122 <bansallakshay081@gmail.com>
2023-01-19 17:19:45 +0530

    Banner Image api

---
+++ routes/bannerImage.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { getAllBannerImage, addBannerImage, deleteBannerImage } = require("../controllers/bannerImageController");
+const { isAuthenticated, authorizedRoles } = require("../middleware/auth");
+
+const router = express.Router();
+
+router.route("/bannerimage/getall").get(isAuthenticated,getAllBannerImage);
+router.route("/bannerimage/add").post(isAuthenticated,authorizedRoles("seller"),addBannerImage);
+router.route("/bannerimage/delete").post(isAuthenticated,authorizedRoles("seller"),deleteBannerImage);
+
+module.exports = router;
