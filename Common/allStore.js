b2126d668f8592018e60ddd8e0412c25e67d5ee6
Lakshay122 <bansallakshay081@gmail.com>
2023-01-25 02:32:10 +0530

    send email

--- Common/allStore.js
+++ Common/allStore.js
@@ -4,7 +4,7 @@ const catchAsyncError = require("../middleware/catchAsyncError");
 
 
 exports.allStore = catchAsyncError(async (req, res, next) => {
-    stores_location = await Location.find({ is_store: true});
+    stores_location = await Location.find({ is_store: true,store_city_title:{$ne:null}});
     res.status(200).json({
         success: true,
         stores_location,
